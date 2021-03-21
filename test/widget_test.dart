58aca718a4c77d9a9aefabaf9bd9ef66f027bd3c
JordanTanReiYao <44694137+JordanTanReiYao@users.noreply.github.com>
2021-03-22 02:38:17 +0800

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Cafe_Bunny/main.dart';
+import 'package:Cafe_Bunny/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
