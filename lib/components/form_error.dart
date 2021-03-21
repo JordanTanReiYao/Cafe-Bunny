918e9ba78070eb5127f3fe52e8cf5e9d66f99b84
JordanTanReiYao <44694137+JordanTanReiYao@users.noreply.github.com>
2021-03-21 22:55:31 +0800

    initial integrate

---
+++ lib/components/form_error.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:Cafe_Bunny/size_config.dart';
+
+class FormError extends StatelessWidget {
+  const FormError({
+    Key key,
+    @required this.errors,
+  }) : super(key: key);
+
+  final List<String> errors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children:
+          List.generate(errors.length, (index) => formErrorText(errors[index])),
+    );
+  }
+
+  Row formErrorText(String error) {
+    return Row(
+      children: [
+        Icon(
+          Icons.error,
+          color: Colors.red,
+          size: 24,
+        ),
+        SizedBox(
+          width: getProportionateScreenWidth(10),
+        ),
+        Text(error),
+      ],
+    );
+  }
+}
